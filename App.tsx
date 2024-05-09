cc1e189f81bd4982ad27bef6ca5965f9dd44e0bc
Danico_Mfala <mfaladan@hotmail.com>
2024-05-09 03:47:59 +0300

    Firebase Auth

--- App.tsx
+++ App.tsx
@@ -1,20 +1,45 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { NavigationContainer } from '@react-navigation/native';
+import Login from './app/screens/Login';
+import Home from './app/screens/Home';
+import { User, onAuthStateChanged } from 'firebase/auth';
+import { useEffect, useState } from 'react';
+import { FIREBASE_AUTH } from './firebaseConfig';
+import Logout from './app/screens/logout';
+
+const Stack = createNativeStackNavigator();
+
+const InsideStack = createNativeStackNavigator();
+
+function InsideLayout() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <InsideStack.Navigator>
+      <InsideStack.Screen name="Home" component={Home} />
+      <InsideStack.Screen name="Logout" component={Logout} />
+    </InsideStack.Navigator>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default function App() {
+  const [user, setUser] = useState<User | null>(null);
+
+  useEffect(() => {
+    onAuthStateChanged(FIREBASE_AUTH, (user) => {
+      console.log('user', user);
+      setUser(user);
+
+    })
+  }, [])
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName='Login'>
+        {user ? (
+          <Stack.Screen name="Inside" component={InsideLayout} options={{ headerShown: false }} />
+        ) : (
+          <Stack.Screen name="Login" component={Login} options={{ headerShown: false }} />
+        )}
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
---
+++ app/screens/Logout.tsx
@@ -0,0 +1,13 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const Logout = () => {
+    return (
+        <View>
+            <Text>Logout
+            </Text>
+        </View>
+    )
+}
+
+export default Logout
