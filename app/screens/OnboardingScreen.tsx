7517c34ca06ae292705ed62c53ae3d5fbcaa73dc
Danico_Mfala <mfaladan@hotmail.com>
2024-05-12 19:15:16 +0300

    onboarding button skip and next fix

--- app/screens/OnboardingScreen.tsx
+++ app/screens/OnboardingScreen.tsx
@@ -2,13 +2,23 @@ import { View, StyleSheet, Dimensions } from 'react-native';
 import React from 'react';
 import Onboarding from 'react-native-onboarding-swiper';
 import LottieView from 'lottie-react-native';
+import { useNavigation } from '@react-navigation/native';
 
-const { width, height } = Dimensions.get('window');
+const { width } = Dimensions.get('window');
 
 export default function OnboardingScreen() {
+    const navigation = useNavigation();
+
+    const handleDone = () => {
+        navigation.navigate('Login');
+    }
+
     return (
         <View style={styles.container}>
             <Onboarding
+                onDone={handleDone}
+                onSkip={handleDone}
+                containerStyles={{ paddingHorizontal: 15 }}
                 pages={[
                     {
                         backgroundColor: '#a7f3d0',
